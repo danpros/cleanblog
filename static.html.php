ac783edd394fe9182657b596f3e837198a0a2459
danpros <dan@danpros.com>
2024-01-11 11:54:09 +0700

    Update static.html.php

--- static.html.php
+++ static.html.php
@@ -6,6 +6,24 @@
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <?php if (login()) { echo tab($p); } ?> 
                 <?php echo $p->body;?>
+				
+                <?php if (!empty($next) || !empty($prev)): ?>
+                <hr>
+                <!-- Pager -->
+                <ul class="pager">
+                    <?php if (!empty($next)): ?>
+                    <li class="next pull-left">
+                        <a href="<?php echo($next['url']); ?>" rel="next">&larr; <?php echo($next['title']); ?></a>
+                    </li>
+                    <?php endif;?>
+                    <?php if (!empty($prev)): ?>
+                    <li class="prev pull-right">
+                        <a href="<?php echo($prev['url']); ?>" rel="prev"><?php echo($prev['title']); ?> &rarr;</a>
+                    </li>
+                    <?php endif;?>
+                </ul>
+                <?php endif; ?>						
+				
             </div>
         </div>
     </div>
